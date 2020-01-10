d2141bd8ee8f004c25b3918e13aadd4e67a29eab
emresert <emresert1233@gmail.com>
2020-01-10 18:30:20 +0300

    Template first

--- Twitter/src/app/app.module.ts
+++ Twitter/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './components/login/login.component';
 import { TweetComponent } from './components/tweet/tweet.component';
 import { TweetDetailComponent } from './components/tweet-detail/tweet-detail.component';
-
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,7 @@ import { TweetDetailComponent } from './components/tweet-detail/tweet-detail.com
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
