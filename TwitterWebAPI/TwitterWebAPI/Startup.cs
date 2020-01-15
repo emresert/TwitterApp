c181cb976f6f0214384170367b6d853648e846ad
emresert <emresert1233@gmail.com>
2020-01-15 12:35:49 +0300

    Authentication is started

--- TwitterWebAPI/TwitterWebAPI/Startup.cs
+++ TwitterWebAPI/TwitterWebAPI/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
 using TwitterWebAPI.Data;
 using TwitterWebAPI.Models;
 
@@ -30,12 +33,26 @@ namespace TwitterWebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
             services.AddCors();
             services.AddDbContext<TwitterAPIContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             services.AddScoped<IAppRepository, AppRepository>();
-
             services.AddAutoMapper();
+
+
+            //  (key) appsettings.json 'da tanımlanmalıdır. 
+            var key = Encoding.ASCII.GetBytes(Configuration.GetSection("Appsettings:Token").Value);
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+               options =>
+               {
+                   options.TokenValidationParameters = new TokenValidationParameters
+                   {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                   };
+               });
 
         }
 
@@ -64,6 +81,8 @@ namespace TwitterWebAPI
             });
 
             app.UseHttpsRedirection();
+
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
